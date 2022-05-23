6054afd82dd0cddc8ad8d374d05665881378f8ac
MihajloESTS <milojevicm374@gmail.com>
2022-05-23 20:02:22 +0200

    sql functions and triggers

--- DATABASE/SQL Scripts/MYSQL/create.sql
+++ DATABASE/SQL Scripts/MYSQL/create.sql
@@ -27,7 +27,7 @@ CREATE TABLE profesori(
 
 CREATE TABLE studenti(
 	broj_indeksa VARCHAR(20) PRIMARY KEY,
-	jmbg VARCHAR(13) UNIQUE CHECK (LENGTH(jmbg) = 13),
+	jmbg VARCHAR(13) UNIQUE NOT NULL CHECK (LENGTH(jmbg) = 13),
 	ime VARCHAR(50) NOT NULL,
 	prezime VARCHAR(50) NOT NULL,
 	mejl VARCHAR(50) NOT NULL UNIQUE,
@@ -52,7 +52,14 @@ CREATE TABLE zavisnosti(
 CREATE TABLE ispiti(
 	id_predmeta NUMERIC NOT NULL REFERENCES predmeti(id_predmeta),
 	broj_indeksa VARCHAR(20) NOT NULL REFERENCES studenti(broj_indeksa),
-	ocena NUMERIC NOT NULL CHECK (ocena BETWEEN 5 AND 10),
+	ocena NUMERIC CHECK (ocena BETWEEN 5 AND 10 OR ocena IS NULL),
+	datum DATETIME NOT NULL,
+	PRIMARY KEY (id_predmeta, broj_indeksa)
+);
+
+CREATE TABLE slusanja(
+	id_predmeta NUMERIC NOT NULL REFERENCES predmeti(id_predmeta),
+	broj_indeksa VARCHAR(20) NOT NULL REFERENCES studenti(broj_indeksa),
 	PRIMARY KEY (id_predmeta, broj_indeksa)
 );
 
@@ -63,6 +70,11 @@ RETURNS INT
 BEGIN
 	DECLARE brojZavisnihPredmeta INT;
     DECLARE brojPolozenihPredmeta INT;
+    DECLARE student_id_smera INT;
+    DECLARE predmet_id_smera INT;
+   	
+    SET student_id_smera = (SELECT id_smera FROM studenti WHERE broj_indeksa = broj_indeksa_in);
+    SET predmet_id_smera = (SELECT id_smera FROM predmeti WHERE id_predmeta = id_predmeta_in);
     
     SET brojZavisnihPredmeta = (SELECT COUNT(*) FROM zavisnosti WHERE id_predmeta_ko = id_predmeta_in);
     SET brojPolozenihPredmeta = (
@@ -72,23 +84,11 @@ BEGIN
         	 id_predmeta IN( SELECT id_predmeta_od FROM zavisnosti WHERE id_predmeta_ko = id_predmeta_in) AND
         	 ocena > 5
         );
-   RETURN brojZavisnihPredmeta = brojPolozenihPredmeta;
+   RETURN brojZavisnihPredmeta = brojPolozenihPredmeta AND student_id_smera = predmet_id_smera;
 END; //
-
-DELIMITER ;
-
-CREATE TABLE slusanja(
-	id_predmeta NUMERIC NOT NULL REFERENCES predmeti(id_predmeta),
-	broj_indeksa VARCHAR(20) NOT NULL REFERENCES studenti(broj_indeksa),
-	PRIMARY KEY (id_predmeta, broj_indeksa),
-	CHECK (mozeDaUpise(broj_indeksa, id_predmeta) = 1)
-);
-
-DELIMITER //
 
 CREATE FUNCTION prestupnaGodina(godina INT)
 RETURNS INT
-
 BEGIN
 	IF((godina % 4 = 0 AND godina % 100 <> 0) OR godina % 400 = 0) THEN
     	RETURN 1;
@@ -96,18 +96,148 @@ BEGIN
     RETURN 0;
 END; //
 
-DELIMITER ;
+CREATE FUNCTION brojDanaUMesecu(mesec INT, godina INT)
+RETURNS INT
+BEGIN
+	CASE
+        WHEN mesec IN(1, 3, 5, 7, 8, 10, 12) THEN RETURN 31;
+        WHEN mesec IN(4, 6, 9, 11) THEN RETURN 30;
+        WHEN mesec = 2 THEN RETURN (28 + prestupnaGodina(godina));
+        ELSE SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = 'INVALID MONTH NUMBER', MYSQL_ERRNO = 1369;
+    END CASE;
+END; //
 
-DELIMITER //
+CREATE FUNCTION godinaIzJMBG(jmbg VARCHAR(13))
+RETURNS INT
+BEGIN
+	DECLARE godina INT;
+    DECLARE prvaCifra INT;
+    
+    SET godina = CAST(SUBSTR(jmbg, 5, 3) AS UNSIGNED);
+    
+    IF SUBSTR(jmbg, 5, 1) = '9' THEN 
+    	SET prvaCifra = 1;
+    ELSE 
+    	SET prvaCifra = 2;
+    END IF;
+    
+    RETURN (prvaCifra * 1000 + godina);
+    
+END; //
+
+CREATE FUNCTION mesecIzJMBG(jmbg VARCHAR(13))
+RETURNS INT
+BEGIN
+    RETURN CAST(SUBSTR(jmbg, 3, 2) AS UNSIGNED);
+    
+END; //
+
+CREATE FUNCTION danIzJMBG(jmbg VARCHAR(13))
+RETURNS INT
+BEGIN
+    RETURN CAST(SUBSTR(jmbg, 1, 2) AS UNSIGNED);
+END; //
+
+CREATE FUNCTION proveraJMBG(jmbg VARCHAR(13))
+RETURNS INT
+BEGIN
+	DECLARE dan INT;
+	DECLARE mesec INT;
+	DECLARE godina INT;
+    
+    SET dan = danIzJMBG(jmbg);
+    SET mesec = mesecIzJMBG(jmbg);
+    SET godina = godinaIzJMBG(jmbg);
+    
+	IF LENGTH(jmbg) <> 13 OR mesec NOT BETWEEN 1 AND 12 OR dan <> brojDanaUMesecu(mesec, godina)
+    	THEN RETURN 0;
+    END IF;
+    RETURN 1;
+END; //
+
+CREATE FUNCTION proveraIspita(broj_indeksa_in VARCHAR(20), id_predmeta_in INT)
+RETURNS INT
+BEGIN
+	RETURN ((SELECT COUNT(*) FROM slusanja WHERE broj_indeksa = broj_indeksa_in AND id_predmeta = id_predmeta_in) = 1);
+END; //
 
 CREATE TRIGGER slusanja_before_insert
 BEFORE INSERT
    ON slusanja FOR EACH ROW
-
 BEGIN
 	IF(mozeDaUpise(NEW.broj_indeksa, NEW.id_predmeta) = 0) THEN
     	SIGNAL SQLSTATE 'HY000'
                  SET MESSAGE_TEXT = 'CHECK CONSTRAINT FOR slusanja FAILED', MYSQL_ERRNO = 1369;
+    END IF;
+END; //
+
+CREATE TRIGGER slusanja_before_update
+BEFORE UPDATE
+   ON slusanja FOR EACH ROW
+BEGIN
+	IF(mozeDaUpise(NEW.broj_indeksa, NEW.id_predmeta) = 0) THEN
+    	SIGNAL SQLSTATE 'HY000'
+                 SET MESSAGE_TEXT = 'CHECK CONSTRAINT FOR slusanja FAILED', MYSQL_ERRNO = 1369;
+    END IF;
+END; //
+
+CREATE TRIGGER profesori_before_insert
+BEFORE INSERT
+ON profesori FOR EACH ROW
+BEGIN
+	IF(proveraJMBG(NEW.jmbg) = 0) THEN
+    	SIGNAL SQLSTATE 'HY000'
+                 SET MESSAGE_TEXT = 'CHECK CONSTRAINT FOR profesori FAILED', MYSQL_ERRNO = 1369;
+    END IF;
+END; //
+
+CREATE TRIGGER profesori_before_update
+BEFORE UPDATE
+ON profesori FOR EACH ROW
+BEGIN
+	IF(proveraJMBG(NEW.jmbg) = 0) THEN
+    	SIGNAL SQLSTATE 'HY000'
+                 SET MESSAGE_TEXT = 'CHECK CONSTRAINT FOR profesori FAILED', MYSQL_ERRNO = 1369;
+    END IF;
+END; //
+
+CREATE TRIGGER studenti_before_insert
+BEFORE INSERT
+ON studenti FOR EACH ROW
+BEGIN
+	IF(proveraJMBG(NEW.jmbg) = 0) THEN
+    	SIGNAL SQLSTATE 'HY000'
+                 SET MESSAGE_TEXT = 'CHECK CONSTRAINT FOR studenti FAILED', MYSQL_ERRNO = 1369;
+    END IF;
+END; //
+
+CREATE TRIGGER studenti_before_update
+BEFORE UPDATE
+ON studenti FOR EACH ROW
+BEGIN
+	IF(proveraJMBG(NEW.jmbg) = 0) THEN
+    	SIGNAL SQLSTATE 'HY000'
+                 SET MESSAGE_TEXT = 'CHECK CONSTRAINT FOR studenti FAILED', MYSQL_ERRNO = 1369;
+    END IF;
+END; //
+
+CREATE TRIGGER ispiti_before_insert
+BEFORE INSERT
+   ON ispiti FOR EACH ROW
+BEGIN
+	IF(proveraIspita(NEW.broj_indeksa, NEW.id_predmeta) = 0) THEN
+    	SIGNAL SQLSTATE 'HY000'
+                 SET MESSAGE_TEXT = 'CHECK CONSTRAINT FOR ispiti FAILED', MYSQL_ERRNO = 1369;
+    END IF;
+END; //
+
+CREATE TRIGGER ispiti_before_update
+BEFORE UPDATE
+   ON ispiti FOR EACH ROW
+BEGIN
+	IF(proveraIspita(NEW.broj_indeksa, NEW.id_predmeta) = 0) THEN
+    	SIGNAL SQLSTATE 'HY000'
+                 SET MESSAGE_TEXT = 'CHECK CONSTRAINT FOR ispiti FAILED', MYSQL_ERRNO = 1369;
     END IF;
 END; //
 
