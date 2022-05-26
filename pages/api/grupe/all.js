139f516f94091f963aa89b5fbe3fec9c878242aa
Mihajlo <milojevicm374@gmail.com>
2022-05-26 12:58:41 +0200

    some update

--- pages/api/grupe/all.js
+++ pages/api/grupe/all.js
@@ -4,6 +4,6 @@ import mysqlLikeMongo from "@mihajlomilojevic/mysql-like-mongo";
 import connection from "../../../DATABASE/connection/connect";
 
 export default async function Fun(req, res) {
-	//const data = await Grupa.find({});
-	res.json(JSON.stringify(connection) /*|| JSON.stringify(data)*/);
+	const data = await Grupa.find({});
+	res.json(JSON.stringify(data) /*|| JSON.stringify(data)*/);
 }
