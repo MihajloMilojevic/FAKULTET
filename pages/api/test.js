9ceb278222108b2518adf63e861af3285181ee1b
Mihajlo <milojevicm374@gmail.com>
2022-06-13 17:39:03 +0200

    ispiti

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,16 +1,14 @@
-import {Admin, Grad, Grupa} from "../../models";
+import {Admin, Grad, Grupa, Korisnik} from "../../models";
 import Auth from "../../middleware/authentication";
 import errorWrapper from "../../middleware/errorWrapper";
 import { StatusCodes } from "http-status-codes";
 import {sviStudenti} from "../../controllers/studenti"
 
 async function Fun(req, res) {
-	const svi = await Grad.find({});
-	svi.sort((a,b) => (a.naziv > b.naziv) ? 1 : ((b.naziv > a.naziv) ? -1 : 0))
-	await Grad.findAndDelete({id_grada: {"<>": 0}});
-	let a = await Grad.find({});
-	console.log(a)
-	await Grad.create(svi);
+	const svi = await Korisnik.query(
+		"SELECT * FROM korisnici"
+	);
+	
 	res.status(StatusCodes.OK).json(svi);
 }
 
